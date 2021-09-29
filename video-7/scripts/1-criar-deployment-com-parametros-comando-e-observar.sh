de8bc2528f866e86e263affce610736563b62a9b
Bruno Lorenço Lopes <bruno.lorenco.lopes@gmail.com>
2021-09-29 10:37:48 -0300

    Corrigir script de deploymnent com arquivo

--- video-7/scripts/1-criar-deployment-com-parametros-comando-e-observar.sh
+++ video-7/scripts/1-criar-deployment-com-parametros-comando-e-observar.sh
@@ -2,4 +2,4 @@
 watch -n 1 kubectl get all
 
 # Em outra aba do terminal, criar deployment usando imagem do NGINX (modo comandos imperativos)
-kubectl create deployment NOME_DO_DEPLOYMENT --image=nginx
+kubectl create deployment server --image=nginx
--- video-7/scripts/3-criar-deployment-arquivo.sh
+++ video-7/scripts/3-criar-deployment-arquivo.sh
@@ -1,8 +1,8 @@
 # Criar arquivo do deployment
-kubectl create deployment server --image=nginx --dry-run=client -o yaml > server.yml
+kubectl create deployment server --image=nginx --dry-run=client -o yaml > server.yaml
 
 # Visualizar conteúdo do arquivo
-cat server.yml
+cat server.yaml
 
 # Implantar deployment com arquivo (modo configuração de objeto declarativa)
-kubectl apply -f server.yml
+kubectl apply -f server.yaml
